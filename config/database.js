0a74a10b727a702279e597c7777a6d64c447e703
ZiyadZK <kakangtea74@gmail.com>
2023-11-10 19:39:04 +0700

    add lil things

--- config/database.js
+++ config/database.js
@@ -1,8 +1,12 @@
 const sequelize = require('sequelize');
 const mysql2 = require('mysql2');
+require('dotenv').config();
+
+const host = `34.101.71.233` || 'localhost';
 
 const db_connect = new sequelize('db_skillshift', 'root', '', {
-    host: '34.101.71.233',
+    host: host,
+    port: 3308,
     dialect: 'mysql'
 });
 
